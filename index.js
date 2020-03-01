660bb0d67d22bf8560f8ab141fba55d42b94f5dc
Chonwai Un <chonwaiun@Chonwai-MacBook-Pro.local>
2020-03-02 03:06:57 +0800

    Add: Added the first query.

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
 'use strict'
+var __importDefault =
+    (this && this.__importDefault) ||
+    function(mod) {
+        return mod && mod.__esModule ? mod : { default: mod }
+    }
 Object.defineProperty(exports, '__esModule', { value: true })
+var neo4j_driver_1 = __importDefault(require('neo4j-driver'))
+var graphenedbURL = 'bolt://localhost:7687'
+var graphenedbUser = 'root'
+var graphenedbPass = 'root'
+var driver = neo4j_driver_1.default.driver(
+    graphenedbURL,
+    neo4j_driver_1.default.auth.basic(graphenedbUser, graphenedbPass)
+)
+var session = driver.session()
+session
+    .run("MATCH (n:User) WHERE n.display_name='Rama' RETURN n")
+    .then(function(result) {
+        result.records.forEach(function(record) {
+            console.log(record._fields)
+        })
+        session.close()
+    })
+    .catch(function(error) {
+        console.log(error)
+    })
--- index.ts
+++ index.ts
@@ -1,2 +1,26 @@
 import express from 'express'
 import neo4j from 'neo4j-driver'
+import request from 'request'
+
+var graphenedbURL: string = 'bolt://localhost:7687'
+var graphenedbUser: string = 'root'
+var graphenedbPass: string = 'root'
+
+var driver: any = neo4j.driver(
+    graphenedbURL,
+    neo4j.auth.basic(graphenedbUser, graphenedbPass)
+)
+
+var session = driver.session()
+session
+    .run(`MATCH (n:User) WHERE n.display_name='Rama' RETURN n`)
+    .then(function(result: any) {
+        result.records.forEach(function(record: any) {
+            console.log(record._fields)
+        })
+
+        session.close()
+    })
+    .catch(function(error: object) {
+        console.log(error)
+    })
