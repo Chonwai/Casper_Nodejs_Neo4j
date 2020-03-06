d8f690f7f681ac7e54aa99ba75a0a4fc9d95ac3b
Chonwai <chonwaiun@gmail.com>
2020-03-07 00:39:39 +0800

    Add: Try the different way to connect and query the neo4j.

--- index.ts
+++ index.ts
@@ -1,27 +1,46 @@
 import express from 'express'
 import neo4j from 'neo4j-driver'
-import Neode from 'neode';
+// import neo4j from 'neo4j'
+import Neode from 'neode'
+import dotenv from 'dotenv'
 
-const instance: Neode = Neode.fromEnv();
+// const instance: Neode = Neode.fromEnv()
 
-var graphenedbURL: string = 'bolt://localhost:7687'
-var graphenedbUser: string = 'root'
-var graphenedbPass: string = 'root'
+// dotenv.config()
+
+var graphenedbURL: any = process.env['NEO4J_URL']
+var graphenedbUser: any = process.env['NEO4J_USERNAME']
+var graphenedbPass: any = process.env['NEO4J_PASSWORD']
+var graphenedbHost: any = process.env['NEO4J_HOST']
+var graphenedbHTTPPort: any = process.env['NEO4J_HTTP_PORT']
 
 var driver: any = neo4j.driver(
-    graphenedbURL,
-    neo4j.auth.basic(graphenedbUser, graphenedbPass)
+    'bolt://localhost:7687',
+    neo4j.auth.basic('neo4j', '1234')
 )
 
 var session = driver.session()
-session
-    .run(`MATCH (n:User) WHERE n.display_name='Rama' RETURN n`)
-    .then(function(result: any) {
-        result.records.forEach(function(record: any) {
-            console.log(record._fields)
-        })
-        session.close()
-    })
-    .catch(function(error: object) {
-        console.log(error)
-    })
+// session
+//     .run(`CREATE (n:Person {name: $nameParam}) RETURN n`, {
+//         nameParam: 'Alice',
+//     })
+//     .then(function(result: any) {
+//         result.records.forEach(function(record: any) {
+//             console.log(record._fields)
+//         })
+//         session.close()
+//     })
+//     .catch(function(error: object) {
+//         console.log(error)
+//     })
+
+async function query() {
+    // var result = await session.run(
+    //     `CREATE (n:Person {name: $nameParam}) RETURN n`,
+    //     { nameParam: 'Tom' }
+    // )
+    var result = await session.run(`MATCH (n:Person) RETURN n`)
+    console.log(result.records)
+}
+
+query()
